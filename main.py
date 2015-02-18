375cfe80d735a75d1df229ee29a1a6e6866ed4c3
Richard Gong <richardgong@gmail.com>
2015-02-17 19:23:29 -0800

    basic landing page

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+app = Flask(__name__,
+            static_folder='assets',
+            static_url_path='',
+            template_folder='assets')
 
-if __name__ == "__main__":
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
     app.run()
