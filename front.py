a14b9b984ef1f7ac7046b0cc7e1f5021033386bb
Richard Gong <richardgong@gmail.com>
2015-02-17 20:32:04 -0800

    form working and database plugged in

---
+++ front.py
@@ -0,0 +1,21 @@
+from flask import render_template
+from flask_wtf import Form
+from wtforms.validators import DataRequired
+
+from main import app
+import wtforms as wtf
+
+
+class SubscribeForm(Form):
+    email = wtf.StringField(label='Your Email', validators=[DataRequired()])
+
+@app.route('/')
+def home_view():
+    return render_template('home.html')
+
+@app.route('/form', methods=['GET', 'POST'])
+def form_view():
+    form = SubscribeForm()
+    if form.validate_on_submit():
+        return "Thanks"
+    return render_template('form.html', form=form)
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__,
             static_folder='assets',
             static_url_path='',
             template_folder='assets')
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+app.debug = os.environ.get('DYNO') is None
+app.secret_key = 'some random key hahahah'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
+
+db = SQLAlchemy(app)
+
+from front import * # load code @UnusedWildImport
+
 
 if __name__ == '__main__':
     app.run()
